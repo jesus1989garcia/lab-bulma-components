8d3c1ed835d2be9ee65510c2ca8592a494ae0aac
jesus1989garcia <jesus1989garcia@gmail.com>
2019-02-23 10:34:53 +0100

    WIP: 2nd done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/navBar';
+import FormField from './components/FormField';
 
 class App extends Component {
 
   
   render() {
     return (
-     NavBar
+      <div>
+         NavBar
+     <div>
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+     <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      
+     </div>
+      </div>
+     
+      
+     
     )
   }
 }
 
 export default App;
+
+
+// render() {
+//   return (
+//     <div>
+//       <div>
+//        NavBar
+//     </div>
+   
+//    <div>
+//      
+//    </div>
+//     </div>
+    
+   
+//   )
+// }
+// }
+
+function validBraces(braces){
+  for ( i = 0; i <= braces; i++ ) {
+if ((braces[i] == braces.length -1)){
+return true;
+}else {
+return false;
+}
+  }
+}
